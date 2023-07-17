08374d6a32f8ad86a72c974c98b2c8051bb29689
Hossain <arafat.kd99@gmail.com>
2023-07-17 19:52:01 +0100

    Update list

--- index.js
+++ index.js
@@ -1,6 +1,36 @@
 import Books from './modules/books.js';
 import dateTime from './modules/luxon.js';
 
+const listLink = document.querySelector('#list-link');
+const addNewLink = document.querySelector('#add-new-link');
+const contactLink = document.querySelector('#contact-link');
+const list = document.querySelector('#lists');
+const addNew = document.querySelector('#add-new');
+const contactUs = document.querySelector('#contact-us');
+
+function enableList() {
+  list.style.display = 'flex';
+  addNew.style.display = 'none';
+  contactUs.style.display = 'none';
+}
+
+function enableAddNew() {
+  list.style.display = 'none';
+  addNew.style.display = 'flex';
+  contactUs.style.display = 'none';
+}
+
+function enableContactUs() {
+  list.style.display = 'none';
+  addNew.style.display = 'none';
+  contactUs.style.display = 'flex';
+}
+
+listLink.addEventListener('click', enableList);
+addNewLink.addEventListener('click', enableAddNew);
+contactLink.addEventListener('click', enableContactUs);
+
+// Date time
 document.querySelector('#date').innerHTML = dateTime;
 const a = new Books();
 a.createObject();
---
+++ modules/list.js
@@ -0,0 +1,8 @@
+// function lists() {
+//   document.getElementById('add-new').style.display = 'none';
+//   document.getElementById('contactus').style.display = 'none';
+//   document.getElementById('lists').style.display = 'flex';
+//   document.getElementById('container').style.display = 'flex';
+// }
+
+// export default lists;
