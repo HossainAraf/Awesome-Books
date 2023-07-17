7b4ac6f941d202632fb68d78ad13441d07630eb5
Hossain <arafat.kd99@gmail.com>
2023-07-17 15:47:21 +0100

    Update time & date format

--- modules/luxon.js
+++ modules/luxon.js
@@ -1,5 +1,4 @@
-
-let DateTime = luxon.DateTime;
-const dateTime = DateTime.now().toString();
-
+/* global luxon */
+const dt = luxon.DateTime.now();
+const dateTime = dt.toFormat('MMMM-dd-yyyy HH:mm a');
 export default dateTime;
