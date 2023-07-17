dae29934a2854be7fb4335c4cf68a4d102ab6815
Hossain <arafat.kd99@gmail.com>
2023-07-17 20:33:15 +0100

    Correct eslint errors

--- modules/list.js
+++
@@ -1,8 +0,0 @@
-// function lists() {
-//   document.getElementById('add-new').style.display = 'none';
-//   document.getElementById('contactus').style.display = 'none';
-//   document.getElementById('lists').style.display = 'flex';
-//   document.getElementById('container').style.display = 'flex';
-// }
-
-// export default lists;
